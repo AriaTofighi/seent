b27be600a16407f57fdf536a5db57990fd6f13b7
AriaTofighi <aria.tofighi1@gmail.com>
2022-11-13 23:36:14 -0800

    Updated env settings

--- frontend/components/navigation/SearchSideBar.tsx
+++ frontend/components/navigation/SearchSideBar.tsx
@@ -1,23 +1,11 @@
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
-import { Box } from "@mui/system";
-import { useRouter } from "next/router";
-import React from "react";
-import { useForm } from "react-hook-form";
-import TextInput from "../controls/TextInput";
+import { useMediaQuery, useTheme } from "@mui/material";
 import SearchBar from "./SearchBar";
-import styles from "./SearchSideBar.styles";
 
 const SearchSideBar = () => {
   const theme = useTheme();
   const mobileMode = useMediaQuery(theme.breakpoints.down("lg"));
 
-  return !mobileMode && <SearchBar />;
+  return <>{!mobileMode && <SearchBar />}</>;
 };
 
 export default SearchSideBar;
