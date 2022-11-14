111b0ee274f3aca6f11e5a92786d535927d29b35
AriaTofighi <aria.tofighi1@gmail.com>
2022-11-13 23:45:24 -0800

    Cors

--- backend/config/cors.config.ts
+++ backend/config/cors.config.ts
@@ -1,5 +1,9 @@
 const corsConfig = {
-  origin: ["http://localhost:3000", "http://localhost:3001"],
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://seent.vercel.app",
+  ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 };
 
