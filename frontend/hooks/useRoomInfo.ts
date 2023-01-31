84f12db613e0df31ad367b735db63ba4c4c5d771
AriaTofighi <aria.tofighi1@gmail.com>
2023-01-31 02:44:58 -0800

    Updated last message display logic

--- frontend/hooks/useRoomInfo.ts
+++ frontend/hooks/useRoomInfo.ts
@@ -3,11 +3,12 @@ import { getLatestMessage } from "../utils";
 
 export const useRoomInfo = (room: any) => {
   const { user } = useUser();
-  if (!room) return {};
-  const latestMessage = getLatestMessage(room);
-  const name = room.roomUsers.length > 2 ? `${latestMessage?.user.name}:` : "";
-  const body = latestMessage?.body ?? "";
 
+  if (!room) return {};
+
+  const latestMessage = getLatestMessage(room);
+  const name = getName(room, user, latestMessage);
+  const body = latestMessage?.body ?? "";
   const previewUser =
     room.roomUsers.length > 2
       ? latestMessage?.user
@@ -17,4 +18,15 @@ export const useRoomInfo = (room: any) => {
   const avatarUrl = previewUser?.images?.[0]?.url;
 
   return { latestMessage, name, body, userId, avatarUrl, username };
+};
+const getName = (room: any, user: any, latestMessage: any) => {
+  const isGroupChat = room.roomUsers.length > 2;
+  const latestMessageIsFromUser = latestMessage?.user?.userId === user?.userId;
+  if (latestMessageIsFromUser) {
+    return "You: ";
+  } else if (isGroupChat) {
+    return latestMessage?.user?.username + ": ";
+  } else {
+    return "";
+  }
 };
