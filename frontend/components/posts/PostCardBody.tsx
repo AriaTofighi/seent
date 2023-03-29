d10f800373449a370b62c87a2d43010972fadfe1
AriaTofighi <aria.tofighi1@gmail.com>
2023-03-28 23:38:28 -0700

    Removed image optimization to fix gif loading

--- frontend/components/posts/PostCardBody.tsx
+++ frontend/components/posts/PostCardBody.tsx
@@ -1,21 +1,18 @@
 import React from "react";
-import Image from "next/image";
+import Image, { ImageLoader, ImageLoaderProps } from "next/image";
 import { Box } from "@mui/system";
 import { Typography } from "@mui/material";
 
-const PostCardBody = ({ body, replyAuthor, image }: any) => {
+const PostCardBody = ({ body, image }: any) => {
+  const contentfulImageLoader: ImageLoader = ({
+    src,
+    width,
+  }: ImageLoaderProps) => {
+    return `${src}?w=${width}`;
+  };
+
   return (
     <Box sx={{ mt: 1 }}>
-      {/* {replyAuthor && (
-        <Typography
-          variant="body1"
-          component="span"o
-          sx={{ mr: 0.5, fontWeight: "bold" }}
-        >
-          @{replyAuthor}
-        </Typography>
-      )} */}
-
       <Typography
         variant="body1"
         sx={{ wordWrap: "break-word", whiteSpace: "normal" }}
@@ -37,6 +34,7 @@ const PostCardBody = ({ body, replyAuthor, image }: any) => {
           }}
         >
           <Image
+            loader={contentfulImageLoader}
             layout="fill"
             objectFit="contain"
             src={image?.url}
